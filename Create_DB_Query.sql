df8c1bff26ed3c34faf27c326fcf299526ecc039
warfront1 <tvcontrolsyou@gmail.com>
2015-12-22 00:02:15 -0500

    Adds Replies
    Adds very basic reply structure

--- Create_DB_Query.sql
+++ Create_DB_Query.sql
@@ -15,9 +15,26 @@ CREATE DATABASE IF NOT EXISTS `image_board` /*!40100 DEFAULT CHARACTER SET utf8 
 USE `image_board`;
 
 
+-- Dumping structure for table image_board.category
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `display_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `abbreviation` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table image_board.category: ~1 rows (approximately)
+DELETE FROM `category`;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` (`id`, `display_name`, `abbreviation`) VALUES
+	(1, 'General', 'G');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+
+
 -- Dumping structure for table image_board.posts
 CREATE TABLE IF NOT EXISTS `posts` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `thread_id` bigint(20) NOT NULL DEFAULT '0',
   `comment` varchar(5000) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `ip_address` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `post_uuid` varchar(36) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
@@ -25,10 +42,28 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `image_size` int(11) DEFAULT NULL,
   `insert_timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `post_uuid` (`post_uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `post_uuid` (`post_uuid`),
+  KEY `thread_id` (`thread_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table image_board.posts: ~0 rows (approximately)
+DELETE FROM `posts`;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+
+
+-- Dumping structure for table image_board.thread
+CREATE TABLE IF NOT EXISTS `thread` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping data for table image_board.thread: ~0 rows (approximately)
+DELETE FROM `thread`;
+/*!40000 ALTER TABLE `thread` DISABLE KEYS */;
+/*!40000 ALTER TABLE `thread` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- image-board/ReactiveImageBoard.py
+++ image-board/ReactiveImageBoard.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 
+from collections import OrderedDict
 from flask import Flask, render_template, request, Response
 import pprint
 import MySQLdb
@@ -11,19 +12,39 @@ app = Flask(__name__)
 static_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'static', 'images')
 ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg', '.gif', '.webm'])
 
-@app.route('/')
-def hello_world():
-    db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
                      passwd="",  # your password
                      db="image_board",
                      cursorclass=MySQLdb.cursors.DictCursor)        # name of the data base
+
+def get_latest_threads():
+# fetches a list of the most recent(last posted on threads)
     cur = db.cursor()
-    cur.execute("""select comment, insert_timestamp, id, post_image, cast(image_size / 1000 as int) 'image_size'
-                    from posts
+    cur.execute("""select p.thread_id
+                    from posts p
+                    where insert_timestamp = (select max(p1.insert_timestamp)
+                                                        from posts p1 where p1.thread_id = p.thread_id)
                     order by insert_timestamp desc
-                    limit 10""")
-    return render_template('main.html', posts=cur.fetchall())
+                    limit 4""")
+    latest_threads = []
+    for result in cur.fetchall():
+        latest_threads.append(int(result['thread_id']))
+    return latest_threads
+
+@app.route('/')
+def hello_world():
+    return_list = []
+    for latest_thread in get_latest_threads():
+        cur = db.cursor()
+        cur.execute("""select thread_id, comment, insert_timestamp, id, post_image, cast(image_size / 1000 as int) 'image_size'
+                        from posts
+                        where thread_id = '%s'
+                        order by insert_timestamp asc
+                        limit 10""",(latest_thread))
+        return_list.append(cur.fetchall())
+    return render_template('main.html', threads=return_list)
 
 @app.route('/post', methods=['POST'])
 def post():
@@ -50,16 +71,23 @@ def post():
         size = os.path.getsize(final_storage_location)
         print size
         print "file upload success"
-        db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                             user="root",         # your username
-                             passwd="",  # your password
-                             db="image_board")        # name of the data base
         cur = db.cursor()
-        cur.execute("""Insert into posts (comment, ip_address, post_uuid, post_image, image_size) values (%s, %s, %s, %s, %s);""",
-                    (request.form['comment'], request.remote_addr, str(post_uuid), storage_location, str(size)))
+        print "reply to id: {0}".format(request.form['reply_to_id'])
+        if int(request.form['reply_to_id']) == -1:
+            # new thread
+            cur.execute("""Insert into thread (category_id) values (%s);""",
+                (1))
+            thread_id =  db.insert_id()
+        else:
+            #reply
+            cur.execute("Select thread_id from posts where id=%s", (request.form['reply_to_id']))
+            thread_id =  int(cur.fetchone()['thread_id'])
+        cur.execute("""Insert into posts (thread_id, comment, ip_address, post_uuid, post_image, image_size) values (%s, %s, %s, %s, %s, %s);""",
+                    (thread_id, request.form['comment'], request.remote_addr, str(post_uuid), storage_location, str(size)))
         db.commit()
     # return Response(status=204)
     return hello_world()
 
 if __name__ == '__main__':
+    # app.debug = True
     app.run(host='0.0.0.0')
